653128ae8b2f8806ff34a88abd4e464e52945e90
arafa95 <32606348+arafa95@users.noreply.github.com>
2017-12-05 12:37:59 +0530

    CAA Academic added
    update database

---
+++ model/caa_academic_model.php
@@ -0,0 +1,31 @@
+<?php
+	if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+        /* 
+           Up to you which header to send, some prefer 404 even if 
+           the files does exist for security
+        */
+        header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+
+        /* choose the appropriate page to redirect users */
+        die( header( 'location: /error.php' ) );
+
+    }
+
+    class CaaAcademicModel{
+      protected static $db;
+
+      function __construct(){
+        self::$db = new DB();
+
+      } 
+
+      function view_CaaAcademic($CaaAcademic_id){
+        $query = "SELECT * FROM `CaaAcademic` WHERE id = ".$CaaAcademicc_id." ";
+
+        $result = self::$db->select($query);
+
+        return $result;
+      }
+
+    }
+?>
