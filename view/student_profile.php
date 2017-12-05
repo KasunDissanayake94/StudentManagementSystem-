c52ff3ef901639883117b9def9eb11a3366f8ba4
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2017-12-05 11:22:08 +0530

    Manage Student Form Completed

--- view/student_profile.php
+++ view/student_profile.php
@@ -1,76 +1,107 @@
-
-<?php 
+<?php
 session_start();
- ?>
- <!DOCTYPE html>
- <html>
- <head>
- 	<title>View Student Profile</title>
- 	<link rel="stylesheet" type="text/css" href="../view/css/style1.css">
-	<script type="text/javascript" src="../view/js/main.js"></script>
- 	
- </head>
-<body>
+?>
+
+<!DOCTYPE html>
+<html>
 
 
+<head>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../view/css/style2.css">
+    <link rel="stylesheet" type="text/css" href="../view/css/style1.css">
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="test/main.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+</head>
 
+<body class="home">
+<div class="display-table">
+    <div class="row display-table-row">
+        <div class="col-md-2 col-sm-1 hidden-xs display-table-cell v-align box" id="navigation">
+            <div class="logo">
+                <a href="home.html"><img src="../view/images/002.jpg" alt="merkery_logo" class="hidden-xs hidden-sm">
+                    <img src="../view/images/002.jpg" alt="merkery_logo" class="visible-xs visible-sm circle-logo">
+                </a>
+            </div>
+            <div class="navi">
+                <ul>
+                    <li><a href="../controller/admin_controller.php"><i class="fa fa-home" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Profile</span></a></li>
+                    <li class="active"><a href="../controller/admin_controller.php?op=Home"><i class="fa fa-tasks" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Home</span></a></li>
+                    <li><a href="../controller/admin_controller.php?op=Modify Students"><i class="fa fa-bar-chart" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Modify Students</span></a></li>
+                    <li><a href="../controller/admin_controller.php?op=Search Students"><i class="fa fa-user" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Search Students</span></a></li>
+                    <li><a href="../controller/admin_controller.php?op=Add Student Details"><i class="fa fa-cog" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Add Student Details</span></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-10 col-sm-11 display-table-cell v-align">
+            <!--<button type="button" class="slide-toggle">Slide Toggle</button> -->
+            <div class="row">
+                <header>
+                    <div class="col-md-7">
 
-<div class="header" id="header">
-    <div id="btn" class="toggle-btn" onclick="togglesidebar()">
-        <span></span>
-        <span></span>
-        <span></span>
+                        <nav class="navbar-default pull-left">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="offcanvas" data-target="#side-menu" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                            </div>
+                        </nav>
+
+                        <div class="title hidden-xs hidden-sm">
+                            <h3>University of Colombo School of Computing</h3>
+                        </div>
+
+                        <!-- <div class="search hidden-xs hidden-sm">
+                            <input type="text" placeholder="Search" id="search">
+                        </div> -->
+                    </div>
+                    <div class="col-md-5">
+                        <div class="header-rightside">
+                            <ul class="list-inline header-top pull-right">
+                                <!-- <li class="hidden-xs"><a href="#" class="add-project" data-toggle="modal" data-target="#add_project">Add Project</a></li> -->
+                                <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+                                <li>
+                                    <a href="#" class="icon-info">
+                                        <i class="fa fa-bell" aria-hidden="true"></i>
+                                        <span class="label label-primary">3</span>
+                                    </a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['type'] ?>
+                                        <b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <div class="navbar-content">
+                                                <span><?php echo $_SESSION['fname'] ?> <?php echo $_SESSION['lname'] ?></span>
+                                                <p class="text-muted small">
+                                                    <?php echo $_SESSION['username'] ?>
+                                                </p>
+                                                <div class="divider">
+                                                </div>
+                                                <a href="../index.php?op=logout" class="view btn-sm active">log out</a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </header>
+            </div>
+
+        </div>
     </div>
-    <span id="logout"><a href="">log out</a></span>
-    <span id="login"><h3>you are logged as   <?php echo $_SESSION['username'] ?></h3></span>
-    <span id="head_name"><h3>UCSC Student Management System</h3></span>
 
 </div>
-	<div class="side-nav" id="sidebar">
-		
-		<nav>
-			<div class="profile_info">
-					<div class="pic"><img src="../view/images/icon.png"></div>
-					<div class="name">Admin</div>
-			</div>
-			<ul>
-				<li>
-					<a href="lecturer.php">
-						<span  class="active_page">Profile</span>
-					</a>
-				</li>
-
-				<li>
-					<a href="../controller/admin_controller.php?op=Home">	
-						<span>Home</span>
-					</a>
-				</li>
-
-				<li>
-					<a href="../controller/admin_controller.php?op=Modify Students">
-						<span>Modify Students</span>
-					</a>
-				</li>
-
-				<li>
-					<a href="../controller/admin_controller.php?op=Search Students">
-						<span>Search Students</span>
-					</a>
-				</li>
-
-				<li>
-					<a href="../controller/admin_controller.php?op=Add Student Details">
-						<span>Add Student Details</span>
-					</a>
-				</li>
-				
-			</ul>
-		</nav>
-		
-	</div>
-	
 
 
 </body>
- </html>
+
+</html>
