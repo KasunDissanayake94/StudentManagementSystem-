79606c7ccf6a0c8c174f551c1b39da7855008c3f
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2017-12-07 08:51:06 +0530

    Live Search for Admin Edited

--- view/search.php
+++ view/search.php
@@ -1,104 +1,171 @@
+<?php
+session_start();
+?>
+
+<?php
+if(isset($_SESSION['value'])){
+
+    foreach ($_SESSION['value'] as $lecturer) {
+        $first_name=$lecturer['first_name'];
+        $last_name=$lecturer['last_name'];
+        $gender=$lecturer['gender'];
+        $dob=$lecturer['dob'];
+        $telephone=$lecturer['telephone'];
+        $email=$lecturer['email'];
+        $education=$lecturer['education'];
+        $research=$lecturer['research'];
+        $courses=$lecturer['courses'];
+        $awards=$lecturer['awards'];
+    }
+}
+?>
+<!DOCTYPE html>
 <html>
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Webslesson Tutorial</title>
-  <script src="../view/js/jquery-3.2.1.min.js"></script>
-  <script src="../view/js/jquery.tabledit.min.js"></script>
-  <script src="../view/js/jquery.tabledit.js"></script>
-  <script src="../view/js/jquery.min.js"></script> 
-              
 
-  <style> 
-  body{
-    background-color:     #fff986;
-  }
-  #result {
-    background-color: #d07cff;
-  }
-  .search{
-    font-size: 50px;
-    float: left;
-    
-  }
-		.masterlist {
-			width: 100%;
-			border-collapse: collapse;
-        }
 
-		.masterlist th {
-			background: #aaa;
-			text-align: left;
-        }
+<head>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="../view/css/style2.css">
+    <link rel="stylesheet" type="text/css" href="../view/css/style1.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="test/main.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="../view/js/jquery-3.2.1.min.js"></script>
+    <script src="../view/js/jquery.tabledit.min.js"></script>
+    <script src="../view/js/jquery.tabledit.js"></script>
+    <script src="../view/js/jquery.min.js"></script>
+</head>
 
-		.masterlist th, .masterlist td {
-			padding: 10px;
-			border-bottom: 1px solid #aaa;
-        }
-        input[type=text] {
-    		width: 200px;
-    		height:30px;
-            -webkit-transition: width 0.4s ease-in-out;
-            transition: width 0.4s ease-in-out;
-		}
+<body class="home">
+<div class="display-table">
+    <div class="row display-table-row">
+        <div class="col-md-2 col-sm-1 hidden-xs display-table-cell v-align box" id="navigation">
+            <div class="logo">
+                <a href="home.html"><img src="../view/images/002.jpg" alt="merkery_logo" class="hidden-xs hidden-sm">
+                    <img src="../view/images/002.jpg" alt="merkery_logo" class="visible-xs visible-sm circle-logo">
+                </a>
+            </div>
+            <div class="navi">
+                <ul>
+                    <li><a href="../controller/lecturer_controller.php"><i class="fa fa-home" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Home</span></a></li>
+                    <li><a href="../controller/lecturer_controller.php?op=view_lecturer"><i class="fa fa-tasks" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Profile</span></a></li>
+                    <li class="active"><a href="../controller/lecturer_controller.php?op=view_student"><i class="fa fa-bar-chart" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Student Details</span></a></li>
+                    <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Subjects</span></a></li>
+                    <li><a href="#"><i class="fa fa-calendar" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Calender</span></a></li>
+                    <li><a href="#"><i class="fa fa-cog" aria-hidden="true"></i><span class="hidden-xs hidden-sm">Reports</span></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-10 col-sm-11 display-table-cell v-align">
+            <!--<button type="button" class="slide-toggle">Slide Toggle</button> -->
+            <div class="row">
+                <header>
+                    <div class="col-md-7">
 
-/* When the input field gets focus, change its width to 100% */	
-		/*input[type=text]:focus {
-		    width: 100%;
-		}
-		.search{
-			font-size: 20px;			
+                        <nav class="navbar-default pull-left">
+                            <div class="navbar-header">
+                                <button type="button" class="navbar-toggle collapsed" data-toggle="offcanvas" data-target="#side-menu" aria-expanded="false">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                            </div>
+                        </nav>
 
-		}*/
+                        <div class="title hidden-xs hidden-sm">
+                            <h3>University of Colombo School of Computing</h3>
+                        </div>
 
-   </style>
+                        <!-- <div class="search hidden-xs hidden-sm">
+                            <input type="text" placeholder="Search" id="search">
+                        </div> -->
+                    </div>
+                    <div class="col-md-5">
+                        <div class="header-rightside">
+                            <ul class="list-inline header-top pull-right">
+                                <!-- <li class="hidden-xs"><a href="#" class="add-project" data-toggle="modal" data-target="#add_project">Add Project</a></li> -->
+                                <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+                                <li>
+                                    <a href="#" class="icon-info">
+                                        <i class="fa fa-bell" aria-hidden="true"></i>
+                                        <span class="label label-primary">3</span>
+                                    </a>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['type'] ?>
+                                        <b class="caret"></b></a>
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <div class="navbar-content">
+                                                <span><?php echo $_SESSION['fname'] ?><?php echo $_SESSION['lname'] ?></span>
+                                                <p class="text-muted small">
+                                                    <?php echo $_SESSION['username'] ?>
+                                                </p>
+                                                <div class="divider">
+                                                </div>
+                                                <a href="../index.php?op=logout" class="view btn-sm active">log out</a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </header>
+            </div>
+            <div class="user-dashboard">
+                <div class="panel-heading">
+                    <h4>
+                        <b>Student Details</b>
+                    </h4>
+                    <label><input type="text" name="search_text" id="search_text" placeholder="Search by Student Details" class="form-control" /></label>
+                </div>
 
- </head>
- <body>
-  <div class="container">
-   <br />
-   <h1 align="center">Search Student</h1><br />
-   <div class="form-group">
-    <div class="input-group">
-     <span class="search"><h><b>Search</b></h></span>
-     <input type="text" name="search_text" id="search_text" placeholder="Search by Student Details" class="form-control" />
+                <div id="result"></div>
 
+            </div>
+
+
+        </div>
     </div>
-   </div>
-   <br />
-   <div id="result"></div>
-  </div>
- </body>
+
+</div>
+
+
+</body>
+
 </html>
 
-
-
 <script>
-$(document).ready(function(){
+    $(document).ready(function(){
 
- load_data();
+        load_data();
 
- function load_data(query)
- {
-  $.ajax({
-   url:"fetch.php",
-   method:"POST",
-   data:{query:query},
-   success:function(data)
-   {
-    $('#result').html(data);
-   }
-  });
- }
- $('#search_text').keyup(function(){
-  var search = $(this).val();
-  if(search != '')
-  {
-   load_data(search);
-  }
-  else
-  {
-   load_data();
-  }
- });
-});
+        function load_data(query)
+        {
+            $.ajax({
+                url:"fetch.php",
+                method:"POST",
+                data:{query:query},
+                success:function(data)
+                {
+                    $('#result').html(data);
+                }
+            });
+        }
+        $('#search_text').keyup(function(){
+            var search = $(this).val();
+            if(search != '')
+            {
+                load_data(search);
+            }
+            else
+            {
+                load_data();
+            }
+        });
+    });
 </script>
